1e84f02a99cf908a8c0f66304106a69ac888a60d
Elena <e.mandrovska@gmail.com>
2021-04-28 14:42:53 +0300

    added templates and markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+import menuList from './data/menu.json';
+import createMenu from './templates/templMenu.hbs';
+
+const refs = {
+     menu: document.querySelector('.js-menu')
+}
+refs.menu.insertAdjacentHTML('beforeend', createMenu(menuList));
