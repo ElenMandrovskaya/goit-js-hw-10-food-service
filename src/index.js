1845b33f6564c2b79deba1da154d53af60e97333
Elena <e.mandrovska@gmail.com>
2021-04-28 16:16:51 +0300

    added change theme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import './styles.css';
 import menuList from './data/menu.json';
 import createMenu from './templates/templMenu.hbs';
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
 const refs = {
-     menu: document.querySelector('.js-menu')
+    menu: document.querySelector('.js-menu'),
+    swchBtn: document.querySelector('.theme-switch__toggle')
 }
 refs.menu.insertAdjacentHTML('beforeend', createMenu(menuList));
+
+function changeTheme(evt) {
+    if (evt.target.checked) {
+        document.body.classList.add(Theme.DARK)
+    }
+    else
+        document.body.classList.remove(Theme.DARK)
+    
+}
+
+refs.swchBtn.addEventListener('click', changeTheme)
